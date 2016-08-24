d7de85d6697fe25520b3690493bb950748db19ce
Tuss4 <tuss4dbfn@gmail.com>
2016-08-24 15:04:55 -0500

    Initial setup. I got the usr model going, going to work on the event model then add in drf.

---
+++ bjjtourneyfinder/usr/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perm(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
