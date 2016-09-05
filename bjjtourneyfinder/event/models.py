c2445d0eb95a381df6bb5a40cc8500388f72b4fc
tuss4 <tuss4dbfn@gmail.com>
2016-09-05 02:34:41 -0500

    Event model fleshing out.

--- bjjtourneyfinder/event/models.py
+++ bjjtourneyfinder/event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    website = models.URLField()
+    early_registration = models.DateField(null=True)
+    reg_registration = models.DateField()
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
