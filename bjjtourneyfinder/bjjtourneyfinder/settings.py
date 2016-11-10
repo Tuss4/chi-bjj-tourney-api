1ff75051620df8fd9c274e4e47f04c0c39c180d8
Tuss4 <tuss4dbfn@gmail.com>
2016-11-10 14:54:22 -0600

    Get rid of brosable api

--- bjjtourneyfinder/bjjtourneyfinder/settings.py
+++ bjjtourneyfinder/bjjtourneyfinder/settings.py
@@ -138,7 +138,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
     'PAGE_SIZE': 25,
